27c655244dae09cc710eaaf0110ea3ed3e4bfffe
Albert Garde <albertsgarde@gmail.com>
2024-03-05 23:27:14 +0100

    :poop: Add timings to sae_mas

--- thesis/sae_mas.py
+++ thesis/sae_mas.py
@@ -1,4 +1,6 @@
 import itertools
+import os
+import time
 
 import datasets  # type: ignore[missingTypeStubs, import-untyped]
 import torch
@@ -7,7 +9,6 @@ from datasets import IterableDataset  # type: ignore[missingTypeStubs]
 from transformer_lens import HookedTransformer  # type: ignore[import]
 
 from thesis.device import Device
-from thesis.mas import html
 from thesis.sae.sae import SparseAutoencoder
 
 from .mas.mas_store import MASStore  # type: ignore[import]
@@ -27,9 +28,12 @@ def main() -> None:
         )
         sample_dataset = SampleDataset(context_size, 256, model, dataset)
 
+        samples_to_check = 32
+
         point = "resid_pre"
-        layer_index = 5
+        layer_index = 3
         layer_id = f"blocks.{layer_index}.hook_resid_pre"
+        # layer_id = f"blocks.{layer_index}.mlp.hook_post"
 
         sae_data = transformer_lens.utils.download_file_from_hf(
             "jacobcd52/gpt2-small-sparse-autoencoders",
@@ -41,7 +45,7 @@ def main() -> None:
         num_sae_features = sae.num_features
 
         mas_store = MASStore(
-            num_samples=32,
+            num_samples=samples_to_check,
             num_features=num_sae_features,
             context_size=context_size,
             sample_length_pre=192,
@@ -53,17 +57,26 @@ def main() -> None:
         activation_scratch = torch.full((context_size, num_sae_features), torch.nan, device=device.torch())
         sae_hook = sae.hook(activation_scratch)
 
+        model_time = 0.0
+        mas_time = 0.0
+        start_time = time.time()
         for _i, sample in itertools.islice(enumerate(sample_dataset), 32):
+            model_start_time = time.time()
             model.run_with_hooks(sample.tokens, fwd_hooks=[sae_hook])
+            model_time += time.time() - model_start_time
+            mas_start_time = time.time()
             mas_store.add_sample(sample, activation_scratch)
+            mas_time += time.time() - mas_start_time
+        end_time = time.time()
+        print(f"Time taken: {end_time - start_time:.2f}s")
+        print(f"Time taken per sample: {(end_time - start_time) / (samples_to_check)*1000:.2f}s")
 
-        feature_samples = mas_store.feature_samples()
-        feature_activations = mas_store.feature_activations()
+        print(f"Model time: {model_time:.2f}s ({model_time/(end_time - start_time)*100:.2f}%)")
+        print(f"MAS time: {mas_time:.2f}s ({mas_time/(end_time - start_time)*100:.2f}%)")
 
-        for neuron_index in range(40):
-            with open(f"outputs/html/sae_mas/{neuron_index}.html", "w", encoding="utf-8") as f:
-                html_str = html.generate_html(model, feature_samples[neuron_index], feature_activations[neuron_index])
-                f.write(html_str)
+        os.makedirs("outputs", exist_ok=True)
+
+        mas_store.save("outputs/sae_mas_store.zip")
 
 
 if __name__ == "__main__":
