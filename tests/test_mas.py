6c7b7bc543c8ddb8ba0aa0ba40d0a19ccb6a547b
Albert Garde <albertsgarde@gmail.com>
2024-05-21 12:19:01 +0200

    :bug: Update for `WeightedSamplesStore`

--- tests/test_mas.py
+++ tests/test_mas.py
@@ -1,7 +1,9 @@
 import datasets  # type: ignore[missingTypeStubs, import-untyped]
+import torch
 from datasets import IterableDataset  # type: ignore[missingTypeStubs]
 from transformer_lens.HookedTransformer import HookedTransformer  # type: ignore[import]
 
+from thesis import hooks
 from thesis.device import get_device  # type: ignore[import]
 from thesis.mas import algorithm
 from thesis.mas.algorithm import MASLayer, MASParams
@@ -9,7 +11,14 @@ from thesis.mas.algorithm import MASLayer, MASParams
 
 def test_mas() -> None:
     params = MASParams(
-        sample_overlap=128, num_max_samples=8, sample_length_pre=96, sample_length_post=32, samples_to_check=16
+        high_activation_weighting=4.0,
+        sample_overlap=128,
+        num_max_samples=16,
+        sample_length_pre=96,
+        sample_length_post=32,
+        samples_to_check=16,
+        seed=0,
+        activation_bins=[x / 10 for x in range(40)],
     )
 
     device = get_device()
@@ -20,12 +29,23 @@ def test_mas() -> None:
         "monology/pile-uncopyrighted", streaming=True, split="train", trust_remote_code=True
     )
 
-    layers = [MASLayer.from_hook_id("blocks.0.mlp.hook_post", 2048)]
+    hook_point = "blocks.0.mlp.hook_post"
+    layers = [MASLayer.from_hook_id(hook_point, 2048)]
 
     mas_store = algorithm.run(model, dataset, layers, params, device)
 
-    samples = mas_store.feature_samples()
-    activations = mas_store.feature_activations()
+    mas_samples = mas_store.feature_samples()
+    mas_activations = mas_store.feature_activations()
 
-    assert samples.shape == (2048, 8, 128)
-    assert activations.shape == (2048, 8, 128)
+    assert mas_samples.shape == (2048, 16, 128)
+    assert mas_activations.shape == (2048, 16, 128)
+
+    assert mas_samples.isfinite().all()
+    assert mas_activations.isfinite().all()
+
+    generator = torch.Generator().manual_seed(5231)
+    test_indices = torch.randint(0, 2048, (16,), generator=generator)
+
+    for i in test_indices:
+        activations = hooks.neuron_activations(model, hook_point, mas_samples[i, :, :], i, device)
+        assert (activations.argmax(dim=1) == mas_activations[i].argmax(dim=1)).all()
